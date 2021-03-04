fd118559ae565c0f4a988794fe9a7c133b3cf7dd
Lazy_Sloth <l.sloth@yandex.ru>
2021-03-04 13:51:17 +0500

    feat: добавить вставку карточек на страницу с помощью JS

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,50 @@
-let content = document.querySelector('.content');
-let profileName = content.querySelector('.profile__name');
-let profileAbout = content.querySelector('.profile__about');
-let buttonEdit = content.querySelector('.profile__button-edit');
-let popup = content.querySelector('.popup');
-let popupName = popup.querySelector('.popup__item_profile_name');
-let popupAbout = popup.querySelector('.popup__item_profile_about');
-let buttonCancel = popup.querySelector('.popup__button-cancel');
-let popupForm = popup.querySelector('.popup__form');
+const content = document.querySelector('.content');
+const profileName = content.querySelector('.profile__name');
+const profileAbout = content.querySelector('.profile__about');
+const buttonEdit = content.querySelector('.profile__button-edit');
+const popup = content.querySelector('.popup');
+const popupName = popup.querySelector('.popup__item_profile_name');
+const popupAbout = popup.querySelector('.popup__item_profile_about');
+const buttonCancel = popup.querySelector('.popup__button-cancel');
+const popupForm = popup.querySelector('.popup__form');
+const elementTemplate = document.querySelector('#element-template').content;
+const elementsList = document.querySelector('.elements__list');
+const elements = [
+  {
+    elementText: 'гора Архыз, Карачаево-Черкессия',
+    elementImageSrc: './images/arkhyz.jpg'
+  },
+  {
+    elementText: 'Полуостров Камчатка',
+    elementImageSrc: './images/kamchatka.jpg'
+  },
+  {
+    elementText: 'Красная поляна, Краснодарский край',
+    elementImageSrc: './images/krasnaya-polyana.jpg'
+  },
+  {
+    elementText: 'Северная Осетия - Алания',
+    elementImageSrc: './images/north-ossetia-alania.jpg'
+  },
+  {
+    elementText: 'Остров Ольхон',
+    elementImageSrc: './images/olkhon-island.jpg'
+  },
+  {
+    elementText: 'Усть-Ленский Заповедник',
+    elementImageSrc: './images/ust-lenskiy-zapovednik.jpg'
+  },
+];
+
+elements.forEach(function (el) {
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+
+  element.querySelector('.element__image').src = el.elementImageSrc;
+  element.querySelector('.element__image').alt = el.elementText;
+  element.querySelector('.element__text').textContent = el.elementText;
+
+  elementsList.append(element);
+})
 
 function disabledPopup() {
   return popup.classList.add('popup_disabled');
