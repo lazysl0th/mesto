3a1c6b0236a52da7ae996deddcad5906f4660420
Lazy_Sloth <l.sloth@yandex.ru>
2021-03-08 18:25:55 +0500

    feat: добавить попап для картинок и плавное открытие/закрытие попапа

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,64 +5,6 @@ const buttonAdd = content.querySelector('.profile__button-add');
 const popup = content.querySelector('.popup');
 const elementsList = content.querySelector('.elements__list');
 
-const popups = [
-  {
-    type: 'image',
-    className: 'popup_type_figure',
-    buttons: [''],
-  },
-  {
-    type: 'form',
-    className: 'popup_type_form',
-    buttons: ['button-edit', 'button-add',]
-  },
-];
-
-const forms = [
-  {
-    formName: 'popupFormEditProfile',
-    formHeading: 'Редактирование профиля',
-    inputName:
-    {
-      1: 'profileName',
-      2: 'profileAbout',
-    },
-    placeholderName:
-    {
-      1: '',
-      2: '',
-    },
-    inputValue:
-    {
-      1: '',
-      2: '',
-    },
-    buttonText: 'Сохранить',
-    buttonName: 'button-edit',
-  },
-  {
-    formName: 'popupFormAddElement',
-    formHeading: 'Новое место',
-    inputName:
-    {
-      1: 'elementText',
-      2: 'elementImage',
-    },
-    placeholderName:
-    {
-      1: 'Название',
-      2: 'Ссылка на картинку',
-    },
-    inputValue:
-    {
-      1: '',
-      2: '',
-    },
-    buttonText: 'Создать',
-    buttonName: 'button-add',
-  },
-];
-
 const elements = [
   {
     elementText: 'гора Архыз, Карачаево-Черкессия',
@@ -90,73 +32,61 @@ const elements = [
   },
 ];
 
-function openPopup (evt) {
+function openPopup(evt) {
   const target = evt.target;
   const buttonCancel = popup.querySelector('.popup__button-cancel');
-  const popupType = popups.filter((popup) => (Array.from(popup.buttons).find((button) => (button.includes(target.name))) || '') === target.name);
-
-  Array.from(popupType).forEach ((type) => (popup.classList.add(type.className)));
+  popup.querySelector('.popup__image').src = '';
+  popup.querySelector('.popup__text').textContent = '';
 
   if (target.name) {
-    createForm(forms, target, popupType);
+    showForm(target);
   }
   else {
-    createFigure(target, popupType)
+    showFigure(target)
   }
   buttonCancel.addEventListener ('click', disabledPopup);
-  return popup.classList.remove('popup_disabled');
+  return popup.classList.add('popup-visible');
 }
 
-function createForm(forms, target, popupType) {
-  const popupFormTemplate = document.querySelector('#popup__form-template').content;
-  const popupForm = popupFormTemplate.querySelector('.popup__content').cloneNode(true);
-  const popupItems = popupForm.querySelectorAll('.popup__item');
-  const popupInputs = Array.from(popupForm.querySelectorAll('.popup__item'));
-  const form = forms.filter((form) => (form.buttonName === target.name));
+function showForm(target) {
+  const form = popup.querySelector(`#${target.name}`);
+  popup.classList.add('popup_type_form');
+  form.classList.add('popup-visible');
 
   if (target.name === 'button-edit') {
-    form[0].inputValue[1] = content.querySelector('.profile__name').textContent;
-    form[0].inputValue[2] = content.querySelector('.profile__about').textContent;
+    form.querySelectorAll('.popup__item')[0].value = content.querySelector('.profile__name').textContent;
+    form.querySelectorAll('.popup__item')[1].value = content.querySelector('.profile__about').textContent;
   }
-
-  form.forEach(function(form){
-    popupForm.name = form.formName;
-    popupForm.querySelector('.popup__heading').textContent = form.formHeading;
-    Array.from(popupForm.querySelectorAll('.popup__item')).forEach(function(input, index) {
-      input.name = form.inputName[index + 1];
-      input.value = form.inputValue[index + 1];
-      input.placeholder = form.placeholderName[index + 1];
-    });
-    popup.prepend(popupForm);
-  });
-  popupForm.addEventListener('submit', formSubmitHandler);
+  form.addEventListener('submit', formSubmitHandler);
 }
 
-function createFigure(target, popupType) {
-  const popupFigureTemplate = document.querySelector('#popup__figure-template').content;
-  const popupFigure = popupFigureTemplate.querySelector('.popup__content').cloneNode(true);
-  popupFigure.querySelector('.popup__image').src = target.src;
-  popupFigure.querySelector('.popup__text').textContent = target.alt;
-  popup.prepend(popupFigure);
+function showFigure(target) {
+  popup.classList.add('popup_type_figure');
+  popup.querySelector('.popup__content_type_figure').classList.add('popup-visible');
+  popup.querySelector('.popup__image').src = target.src;
+  popup.querySelector('.popup__text').textContent = target.alt;
 }
 
 function disabledPopup() {
-  const popupTypeClass = popups.map((popup) => (popup.className));
-  popupTypeClass.forEach((className) => (popup.classList.remove(className)));
-  /*popup.querySelector('.popup__content').remove();*/
-  return popup.classList.add('popup_disabled');
+  const formItems = popup.querySelectorAll('.popup__item');
+  formItems.forEach((item) => (item.value = ''));
+  popup.classList.remove('popup_type_form');
+  popup.classList.remove('popup_type_figure');
+  popup.querySelectorAll('.popup__content').forEach((content) => (content.classList.remove('popup-visible')));
+  return popup.classList.remove('popup-visible');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  const popupItems = popup.querySelectorAll('.popup__item');
-  popupInputs = Array.from(popupItems).reduce((previousValue, item) => ({[previousValue.name]: previousValue.value, [item.name]: item.value,}));
-  if (popup.querySelector('.popup__content').name === 'popupFormEditProfile') {
-      content.querySelector('.profile__name').textContent = popupInputs.profileName;
-      content.querySelector('.profile__about').textContent = popupInputs.profileAbout;
+  const form = evt.target;
+  const formItems = form.querySelectorAll('.popup__item');
+  formInputs = Array.from(formItems).reduce((previousValue, item) => ({[previousValue.name]: previousValue.value, [item.name]: item.value,}));
+  if (form.name === 'popupFormEditProfile') {
+      content.querySelector('.profile__name').textContent = formInputs.profileName;
+      content.querySelector('.profile__about').textContent = formInputs.profileAbout;
     }
   else {
-    addElement(popupInputs)
+    addElement(formInputs)
   }
   disabledPopup();
 }
@@ -174,12 +104,12 @@ function openElement(elements) {
   })
 }
 
-function addElement(popupInputs) {
+function addElement(formInputs) {
   const newElement = elementTemplate.querySelector('.element').cloneNode(true);
   newElement.querySelector('.element__button-delete').addEventListener('click', deleteElement);
-  newElement.querySelector('.element__image').src = popupInputs.elementImage;
-  newElement.querySelector('.element__image').alt = popupInputs.elementText;
-  newElement.querySelector('.element__text').textContent = popupInputs.elementText;
+  newElement.querySelector('.element__image').src = formInputs.elementImage || '';
+  newElement.querySelector('.element__image').alt = formInputs.elementText || '';
+  newElement.querySelector('.element__text').textContent = formInputs.elementText || '';
   newElement.querySelector('.element__image').addEventListener('click', openPopup);
   newElement.querySelector('.element__button-like').addEventListener('click', likeElement);
   elementsList.prepend(newElement);
