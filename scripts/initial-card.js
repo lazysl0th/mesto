3d589ae9fe6b6d2e621626985195c936d7818c87
Lazy_Sloth <l.sloth@yandex.ru>
2021-03-10 23:07:28 +0500

    refactor: выполнить рефактор функций открытия, закрытия попапа, открытия, создания элемента, удаления элемента

---
+++ scripts/initial-card.js
@@ -0,0 +1,26 @@
+const initialCards  = [
+  {
+    name: 'гора Архыз, Карачаево-Черкессия',
+    link: './images/arkhyz.jpg',
+  },
+  {
+    name: 'Полуостров Камчатка',
+    link: './images/kamchatka.jpg',
+  },
+  {
+    name: 'Красная поляна, Краснодарский край',
+    link: './images/krasnaya-polyana.jpg',
+  },
+  {
+    name: 'Северная Осетия - Алания',
+    link: './images/north-ossetia-alania.jpg',
+  },
+  {
+    name: 'Остров Ольхон',
+    link: './images/olkhon-island.jpg',
+  },
+  {
+    name: 'Усть-Ленский Заповедник',
+    link: './images/ust-lenskiy-zapovednik.jpg',
+  },
+];
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,118 +1,27 @@
-const elementTemplate = document.querySelector('#element-template').content;
 const content = document.querySelector('.content');
 const buttonEdit = content.querySelector('.profile__button-edit');
 const buttonAdd = content.querySelector('.profile__button-add');
-const popup = content.querySelector('.popup');
+const popupEditProfile = content.querySelector('.popup_form_edit-profile');
+const formEditProfile = popupEditProfile.querySelector('.popup__content_type_form');
+const buttonCancelEditProfile = popupEditProfile.querySelector('.popup__button-cancel_type_edit-profile');
+const popupAddElement = content.querySelector('.popup_form_add-element');
+const formAddElement = popupAddElement.querySelector('.popup__content_type_form');
+const buttonCancelAddElement = popupAddElement.querySelector('.popup__button-cancel_type_add-element');
+const popupFigure = content.querySelector('.popup_type_figure');
+const popupFigureImage = content.querySelector('.popup__image');
+const popupFigcap = content.querySelector('.popup__text');
+const buttonCancelFigure = popupFigure.querySelector('.popup__button-cancel');
 const elementsList = content.querySelector('.elements__list');
+const elementTemplate = document.querySelector('#element-template').content;
+const element = elementTemplate.querySelector('.element');
 
-const elements = [
-  {
-    elementText: 'гора Архыз, Карачаево-Черкессия',
-    elementImage: './images/arkhyz.jpg',
-  },
-  {
-    elementText: 'Полуостров Камчатка',
-    elementImage: './images/kamchatka.jpg',
-  },
-  {
-    elementText: 'Красная поляна, Краснодарский край',
-    elementImage: './images/krasnaya-polyana.jpg',
-  },
-  {
-    elementText: 'Северная Осетия - Алания',
-    elementImage: './images/north-ossetia-alania.jpg',
-  },
-  {
-    elementText: 'Остров Ольхон',
-    elementImage: './images/olkhon-island.jpg',
-  },
-  {
-    elementText: 'Усть-Ленский Заповедник',
-    elementImage: './images/ust-lenskiy-zapovednik.jpg',
-  },
-];
 
-function openPopup(evt) {
-  const target = evt.target;
-  const buttonCancel = popup.querySelector('.popup__button-cancel');
-  popup.querySelector('.popup__image').src = '';
-  popup.querySelector('.popup__text').textContent = '';
-
-  if (target.name) {
-    showForm(target);
-  }
-  else {
-    showFigure(target)
-  }
-  buttonCancel.addEventListener ('click', disabledPopup);
-  return popup.classList.add('popup-visible');
+function showPopup(popup) {
+  popup.classList.add('popup-visible');
 }
 
-function showForm(target) {
-  const form = popup.querySelector(`#${target.name}`);
-  popup.classList.add('popup_type_form');
-  form.classList.add('popup-visible');
-
-  if (target.name === 'button-edit') {
-    form.querySelectorAll('.popup__item')[0].value = content.querySelector('.profile__name').textContent;
-    form.querySelectorAll('.popup__item')[1].value = content.querySelector('.profile__about').textContent;
-  }
-  form.addEventListener('submit', formSubmitHandler);
-}
-
-function showFigure(target) {
-  popup.classList.add('popup_type_figure');
-  popup.querySelector('.popup__content_type_figure').classList.add('popup-visible');
-  popup.querySelector('.popup__image').src = target.src;
-  popup.querySelector('.popup__text').textContent = target.alt;
-}
-
-function disabledPopup() {
-  const formItems = popup.querySelectorAll('.popup__item');
-  formItems.forEach((item) => (item.value = ''));
-  popup.classList.remove('popup_type_form');
-  popup.classList.remove('popup_type_figure');
-  popup.querySelectorAll('.popup__content').forEach((content) => (content.classList.remove('popup-visible')));
-  return popup.classList.remove('popup-visible');
-}
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  const form = evt.target;
-  const formItems = form.querySelectorAll('.popup__item');
-  formInputs = Array.from(formItems).reduce((previousValue, item) => ({[previousValue.name]: previousValue.value, [item.name]: item.value,}));
-  if (form.name === 'popupFormEditProfile') {
-      content.querySelector('.profile__name').textContent = formInputs.profileName;
-      content.querySelector('.profile__about').textContent = formInputs.profileAbout;
-    }
-  else {
-    addElement(formInputs)
-  }
-  disabledPopup();
-}
-
-function openElement(elements) {
-  elements.forEach(function (el) {
-    const element = elementTemplate.querySelector('.element').cloneNode(true);
-    element.querySelector('.element__button-delete').addEventListener('click', deleteElement);
-    element.querySelector('.element__image').src = el.elementImage;
-    element.querySelector('.element__image').alt = el.elementText;
-    element.querySelector('.element__text').textContent = el.elementText;
-    element.querySelector('.element__image').addEventListener('click', openPopup);
-    element.querySelector('.element__button-like').addEventListener('click', likeElement);
-    elementsList.append(element);
-  })
-}
-
-function addElement(formInputs) {
-  const newElement = elementTemplate.querySelector('.element').cloneNode(true);
-  newElement.querySelector('.element__button-delete').addEventListener('click', deleteElement);
-  newElement.querySelector('.element__image').src = formInputs.elementImage || '';
-  newElement.querySelector('.element__image').alt = formInputs.elementText || '';
-  newElement.querySelector('.element__text').textContent = formInputs.elementText || '';
-  newElement.querySelector('.element__image').addEventListener('click', openPopup);
-  newElement.querySelector('.element__button-like').addEventListener('click', likeElement);
-  elementsList.prepend(newElement);
+function hidePopup(popup) {
+  popup.classList.remove('popup-visible');
 }
 
 function likeElement(evt) {
@@ -120,13 +29,78 @@ function likeElement(evt) {
 }
 
 function deleteElement(evt) {
-  const element = evt.target.closest(`.${evt.target.parentElement.className}`)
-  element.remove();
+  evt.target.closest('.element').remove();
 }
 
-openElement(elements);
+function showFormEditProfile() {
+  showPopup(popupEditProfile);
+  popupEditProfile.querySelectorAll('.popup__item')[0].value = content.querySelector('.profile__name').textContent;
+  popupEditProfile.querySelectorAll('.popup__item')[1].value = content.querySelector('.profile__about').textContent;
+}
 
-buttonEdit.addEventListener('click', openPopup);
+function showFormAddElement() {
+  showPopup(popupAddElement);
+}
 
-buttonAdd.addEventListener('click', openPopup);
+function showFigure(card) {
+  popupFigureImage.src = card.link;
+  popupFigcap.textContent = card.name;
+  showPopup(popupFigure);
+}
+
+function createElement(card) {
+  const newElement = element.cloneNode(true);
+  const buttonDelete = newElement.querySelector('.element__button-delete');
+  const elementImage = newElement.querySelector('.element__image');
+  const elementText = newElement.querySelector('.element__text');
+  const buttonLike = newElement.querySelector('.element__button-like');
+
+  elementImage.src = card.link;
+  elementImage.alt = card.name;
+  elementText.textContent = card.name;
+
+  buttonDelete.addEventListener('click', deleteElement);
+  elementImage.addEventListener('click', () => showFigure(card));
+  buttonLike.addEventListener('click', likeElement);
+  return newElement;
+}
+
+function openElement(card, place) {
+  place.append(createElement(card));
+}
+
+function formEditSubmitHandler(evt) {
+  evt.preventDefault();
+  const formItems = formEditProfile.querySelectorAll('.popup__item');
+  formInputs = Array.from(formItems).reduce((previousValue, item) => ({[previousValue.name]: previousValue.value, [item.name]: item.value,}));
+  content.querySelector('.profile__name').textContent = formInputs.profileName;
+  content.querySelector('.profile__about').textContent = formInputs.profileAbout;
+  console.log(formEditProfile);
+  hidePopup(popupEditProfile);
+}
+
+function formAddSubmitHandler(evt) {
+  evt.preventDefault();
+  const formItems = formAddElement.querySelectorAll('.popup__item');
+  formInputs = Array.from(formItems).reduce((previousValue, item) => ({[previousValue.name]: previousValue.value, [item.name]: item.value,}));
+  elementsList.prepend(createElement(formInputs));
+  formItems.forEach((item) => (item.value = ''));
+  hidePopup(popupAddElement);
+}
+
+initialCards.forEach((card) => (openElement(card, elementsList)));
+
+buttonEdit.addEventListener('click', showFormEditProfile);
+
+buttonAdd.addEventListener('click', showFormAddElement);
+
+formEditProfile.addEventListener('submit', formEditSubmitHandler);
+
+formAddElement.addEventListener('submit', formAddSubmitHandler);
+
+buttonCancelEditProfile.addEventListener ('click', () => hidePopup(popupEditProfile));
+
+buttonCancelAddElement.addEventListener ('click', () => hidePopup(popupAddElement));
+
+buttonCancelFigure.addEventListener ('click', () => hidePopup(popupFigure));
 
